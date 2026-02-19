bf1624a8ae2d4737d0f79e6d36a3ce34f11351d6
Max Wick <max@x132.de>
2026-02-19 05:09:55 +0100

    style: apply spotless formatting to FieldDefinitionTypeAdapterFactory

--- objectmerger-spring-boot/src/main/java/de/x132/objectmerger/util/FieldDefinitionTypeAdapterFactory.java
+++ objectmerger-spring-boot/src/main/java/de/x132/objectmerger/util/FieldDefinitionTypeAdapterFactory.java
@@ -16,48 +16,49 @@ import java.io.IOException;
 
 public class FieldDefinitionTypeAdapterFactory implements TypeAdapterFactory {
 
+  @Override
+  @SuppressWarnings("unchecked")
+  public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+    if (type.getRawType() != FieldDefinition.class) {
+      return null;
+    }
+    return (TypeAdapter<T>) new FieldDefinitionAdapter(gson).nullSafe();
+  }
+
+  private static class FieldDefinitionAdapter extends TypeAdapter<FieldDefinition<?>> {
+    private final Gson gson;
+    private final TypeAdapter<JsonElement> elementAdapter;
+
+    private FieldDefinitionAdapter(Gson gson) {
+      this.gson = gson;
+      this.elementAdapter = gson.getAdapter(JsonElement.class);
+    }
+
     @Override
-    @SuppressWarnings("unchecked")
-    public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
-        if (type.getRawType() != FieldDefinition.class) {
-            return null;
-        }
-        return (TypeAdapter<T>) new FieldDefinitionAdapter(gson).nullSafe();
+    public void write(JsonWriter out, FieldDefinition<?> value) {
+      throw new UnsupportedOperationException("Serialization to FieldDefinition not supported");
     }
 
-    private static class FieldDefinitionAdapter extends TypeAdapter<FieldDefinition<?>> {
-        private final Gson gson;
-        private final TypeAdapter<JsonElement> elementAdapter;
+    @Override
+    public FieldDefinition<?> read(JsonReader in) throws IOException {
+      JsonElement jsonElement = elementAdapter.read(in);
+      JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-        private FieldDefinitionAdapter(Gson gson) {
-            this.gson = gson;
-            this.elementAdapter = gson.getAdapter(JsonElement.class);
+      String strategy =
+          jsonObject.has("strategy") ? jsonObject.get("strategy").getAsString() : "standard";
+
+      Class<? extends FieldDefinition> targetClass = StandardFieldDefinition.class;
+
+      MergeStrategy<?, ?> mergeStrategy = StrategyRegistry.getInstance().getStrategy(strategy);
+
+      if (mergeStrategy != null) {
+        Class<? extends FieldDefinition> configClass = mergeStrategy.getConfigurationClass();
+        if (configClass != FieldDefinition.class) {
+          targetClass = configClass;
         }
+      }
 
-        @Override
-        public void write(JsonWriter out, FieldDefinition<?> value) {
-            throw new UnsupportedOperationException("Serialization to FieldDefinition not supported");
-        }
-
-        @Override
-        public FieldDefinition<?> read(JsonReader in) throws IOException {
-            JsonElement jsonElement = elementAdapter.read(in);
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
-
-            String strategy = jsonObject.has("strategy") ? jsonObject.get("strategy").getAsString() : "standard";
-
-            Class<? extends FieldDefinition> targetClass = StandardFieldDefinition.class;
-
-            MergeStrategy<?, ?> mergeStrategy = StrategyRegistry.getInstance().getStrategy(strategy);
-
-            if (mergeStrategy != null) {
-                Class<? extends FieldDefinition> configClass = mergeStrategy.getConfigurationClass();
-                if (configClass != FieldDefinition.class) {
-                    targetClass = configClass;
-                }
-            }
-
-            return gson.getAdapter(targetClass).fromJsonTree(jsonElement);
-        }
+      return gson.getAdapter(targetClass).fromJsonTree(jsonElement);
     }
+  }
 }
